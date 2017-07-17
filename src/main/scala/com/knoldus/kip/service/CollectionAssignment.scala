48df21dc5a801b2234fb622a52e791301ac2f37a
ayush <ayush.tiwari@knoldus.in>
2017-07-17 22:45:46 +0530

    Submission of the Assignment

--- src/main/scala/com/knoldus/kip/service/CollectionAssignment.scala
+++ src/main/scala/com/knoldus/kip/service/CollectionAssignment.scala
@@ -1,17 +1,103 @@
 package com.knoldus.kip.service
 
-import com.knoldus.kip.models.ScoreCard
+import com.knoldus.kip.RamDatabase
+import com.knoldus.kip.models.Gender._
+import com.knoldus.kip.models.{Marks, ScoreCard, Student}
 
 trait CollectionAssignment {
 
+  def computeScorecard(id: Long): ScoreCard = {
+    val list: List[Marks] = RamDatabase.marksList.filter(id == _.studentId)
+
+    val subjectList : List[Long] = list.map(_.subjectId.toLong)
+    val subjectsCount: Int = subjectList.length
+
+    val marksList: List[Float] = list.map(_.marksObtained)
+
+    val percentage: Float = marksList.sum / subjectsCount
+
+    val map: Map[Long, Float] = subjectList zip marksList toMap
+
+    ScoreCard(id, map, percentage)
+  }
+
+
   //Collection Based - Assignment 1
-  def generateScorecards: Map[String, AnyRef] = ???
-  def getScorecardsByName(name: String): List[ScoreCard] = ???
+  def generateScorecards: Map[String, AnyRef] = {
+
+    val names: List[String] = RamDatabase.studentList.map(_.name)
+
+    val scorecards: List[ScoreCard] = for{ s <- RamDatabase.studentList}
+      yield computeScorecard(s.id)
+
+    names zip scorecards toMap
+  }
+
+  def getScorecardsByName(name: String): List[ScoreCard] = {
+
+    val students: List[Student] = RamDatabase.studentList.filter(_.name == name).sortBy(_.id)
+    val scorecardList: List[ScoreCard] = for{s <- students
+                                              }yield computeScorecard(s.id)
+
+    if(scorecardList.isEmpty) {
+
+      throw new Exception("No Data Found")
+    }
+    else {
+      scorecardList
+    }
+
+  }
 
   //Collection Based - Assignment 2
-  def getScoreCardByGender: (List[ScoreCard], List[ScoreCard]) = ???
-  def getScoreCardsWithHigherPercentage: (List[ScoreCard], List[ScoreCard]) = ??? //Internally calls getScoreCardByGender
-  def getSimilarPercentageBwGroups: List[((String, ScoreCard), (String, ScoreCard))] = ???
-  def femalePercentageNotInMales: List[(String, ScoreCard)] = ???
+  def getScoreCardByGender: (List[ScoreCard], List[ScoreCard]) = {
+
+    val maleStudents: List[Student] = RamDatabase.studentList.filter(_.gender == MALE).sortBy(_.id)
+    val femaleStudents: List[Student] = RamDatabase.studentList.filter(_.gender == FEMALE).sortBy(_.id)
+
+    val maleScorecard: List[ScoreCard] = for{s <- maleStudents
+    }yield computeScorecard(s.id)
+
+    val femaleScorecard: List[ScoreCard] = for{s <- femaleStudents
+    }yield computeScorecard(s.id)
+
+    (maleScorecard, femaleScorecard)
+  }
+
+  def getScoreCardsWithHigherPercentage: (List[ScoreCard], List[ScoreCard]) = {
+    val t:(List[ScoreCard], List[ScoreCard]) = getScoreCardByGender
+
+    val maleScorecard: List[ScoreCard] = t._1.filter(_.percentage >= 50)
+    val femaleScorecard: List[ScoreCard] = t._2.filter(_.percentage >= 50)
+
+    (maleScorecard, femaleScorecard)
+
+  }
+
+  def getSimilarPercentageBwGroups: List[((String, ScoreCard), (String, ScoreCard))] = {
+
+    def getStudentName(st_Id : Long):String = RamDatabase.studentList.find(_.id==st_Id).get.name
+
+    val (mScoreCard,fScoreCard)  =  getScoreCardByGender
+
+    mScoreCard.flatMap(x => fScoreCard.filter(_.percentage==x.percentage).map(y=>
+      ((getStudentName(x.studentId),x),(getStudentName(y.studentId),y))))
+  }
+
+  def femalePercentageNotInMales: List[(String, ScoreCard)] = {
+    val t:(List[ScoreCard], List[ScoreCard]) = getScoreCardByGender
+
+    val maleScorecard: List[ScoreCard] = t._1
+    val femaleScorecard: List[ScoreCard] = t._2
+
+    val selectedFemales: List[ScoreCard] =for{f <- femaleScorecard
+                                              m <- maleScorecard
+                                             if f.percentage != m.percentage
+    }yield f
+
+    val names: List[String] = selectedFemales.flatMap(x => RamDatabase.studentList.filter(x.studentId == _.id).map(_.name))
+
+    names.zip(selectedFemales)
+  }
 
 }
--- src/main/scala/com/knoldus/kip/service/ListAssignment.scala
+++ src/main/scala/com/knoldus/kip/service/ListAssignment.scala
@@ -1,26 +1,230 @@
 package com.knoldus.kip.service
 
-import com.knoldus.kip.models.Student
+import com.knoldus.kip.RamDatabase
+import com.knoldus.kip.models.{Marks, Student}
 
 trait ListAssignment {
 
   //Assignment -1
-  def failedStudents(subjectId: Long, percentage: Double, passOrFail: String): Int = ???
-  def topBottomStudents(subjectId: Long, count: Int, topOrBottom: String): List[Student] = ???
-  def topAndLeastScorers(topOrBottom: String, count: Int): List[Student] = ???
-  def getScholarshipGroups(percentage: Float, goodScholarship: Int, normalScholarship: Int)
-  : (List[(Student, Int)], List[(Student, Int)]) = ???
-  def passedOrFailed(passOrFail: String, percentage: Float): List[Student] = ???
-  def studentsWithMoreThan95: List[Student] = ???
-  def generateReport: List[(String, List[Int])] = ??? //Must use the groupBy() Method of the List
+  def failedStudents(subjectId: Long, percentage: Double, passOrFail: String): Int = {
+
+    val passCount = RamDatabase.marksList.count(marks => marks.subjectId == subjectId && marks.marksObtained >= percentage)
+    val failCount = RamDatabase.marksList.count(marks => marks.subjectId == subjectId && marks.marksObtained < percentage)
+
+    if(passOrFail.toLowerCase.equals("pass")){
+
+      passCount
+
+    }
+
+    else {
+
+      failCount
+
+    }
+  }
+
+
+  def topBottomStudents(subjectId: Long, count: Int, topOrBottom: String): List[Student] = {
+
+    val sortedMarksList:List[Marks]  = RamDatabase.marksList.filter(_.subjectId == subjectId).sortBy(_.marksObtained)
+
+    def listStudents(marks: List[Marks]) : List[Student] = {
+
+      for {mark <- marks
+           student <- RamDatabase.studentList
+           if student.id == mark.studentId
+      } yield student
+
+    }
+
+    topOrBottom.toLowerCase match {
+
+      case "top" => listStudents(sortedMarksList.reverse.take(count))
+      case "bottom" => listStudents(sortedMarksList.take(count))
+
+    }
+  }
+
+
+  def topAndLeastScorers(topOrBottom: String, count: Int): List[Student] = {
+
+  val totalMarks : List[Float] = {
+
+    for(student <- RamDatabase.studentList)
+
+      yield RamDatabase.marksList.takeWhile(_.studentId == student.id).map(_.marksObtained).sum
+
+  }
+
+   val studentNameWithMarks = RamDatabase.studentList.zip(totalMarks).sortBy(_._2)
+
+    topOrBottom.toLowerCase match{
+
+      case "top" => studentNameWithMarks.reverse.take(count).map(_._1)
+      case "bottom" => studentNameWithMarks.take(count).map(_._1)
+
+    }
+
+  }
+
+
+def getScholarshipGroups(percentage: Float, goodScholarship: Int, normalScholarship: Int): (List[(Student, Int)], List[(Student, Int)]) = {
+
+  val inputPercentage = percentage*5
+
+  val studentIdWithMarks = RamDatabase.studentList.map(_.id).zip(RamDatabase.studentList.map(x => RamDatabase.marksList.
+    filter(x.id == _.studentId).map(_.marksObtained).sum))
+
+  val (studentIdWithGoodScholarship,studentIdWithNormalScholarship) = studentIdWithMarks.partition(_._2>=inputPercentage)
+
+
+  val studentWithGoodScholarship = RamDatabase.studentList.filter(studentIdWithGoodScholarship.map(_._1) contains _.id)
+
+  val studentWithNormalScholarship = RamDatabase.studentList.filter(studentIdWithNormalScholarship.map(_._1) contains _.id)
+
+  val listOfGoodScholarship = List.fill(studentWithGoodScholarship.length)(goodScholarship)
+
+  val listOfNormalScholarship = List.fill(studentWithNormalScholarship.length)(normalScholarship)
+
+  val zippedListGoodScholarship = studentWithGoodScholarship zip listOfGoodScholarship
+
+  val zippedListNormalScholarship = studentWithNormalScholarship zip listOfNormalScholarship
+  (zippedListGoodScholarship, zippedListNormalScholarship)
+}
+
+
+  def passedOrFailed(passOrFail: String, percentage: Float): List[Student] = {
+
+    val totalMarks:List[Float]={
+
+      for(student<- RamDatabase.studentList)
+        yield RamDatabase.marksList.takeWhile(_.studentId==student.id).map(_.marksObtained).sum
+    }
+
+    val percentages=totalMarks.map(_/5)
+
+    passOrFail.toLowerCase match {
+
+      case "pass"=>val studentNameMorePercentage=RamDatabase.studentList.zip(percentages).filter(_._2>=percentage)
+        studentNameMorePercentage.unzip._1
+
+      case "fail"=>
+        val studentNameLessPercentage=RamDatabase.studentList.zip(percentages).filter(_._2<percentage)
+
+        studentNameLessPercentage.unzip._1
+
+    }
+
+  }
+
+
+  def studentsWithMoreThan95: List[Student] = {
+
+    val totalMarks:List[Float]={
+
+      for(student<- RamDatabase.studentList)
+        yield RamDatabase.marksList.takeWhile(_.studentId==student.id).map(_.marksObtained).sum
+    }
+
+    val percentage=totalMarks.map(_/5)
+
+    val studentNameWithMarks=RamDatabase.studentList.zip(percentage).filter(_._2>95)
+
+    studentNameWithMarks.unzip._1
+
+  }
+
+  def generateReport: List[(String, List[Int])] = {  //Must use the groupBy() Method of the List
+
+    val marks: List[List[Int]] = RamDatabase.studentList.map(x => RamDatabase.marksList.groupBy(_.studentId == x.id)(true)
+      .map(_.marksObtained.toInt))
+
+    val student_name = RamDatabase.studentList.map(_.name)
+
+    student_name.zip(marks)
+
+  }
+
+
+
+
+
 
   //Assignment - 2
-  def getLastElementWithIndex(list: List[String]): (String, Int) = ???
-  def printTable(list: List[Long]): List[Long] = ???
-  def aggregateLists(list1: List[String], list2: List[Long]): List[List[(String, Long)]] = ???
-  def getSumOfList(list: List[Long]): Long = ???
-  def getMultiplicationOfList(list: List[Long]) : Long = ???
-  def quickSortList(list: List[Long]): Long = ???
-  def mergeSortList(list: List[Long]): Long = ???
+  def getLastElementWithIndex(list: List[String]): (String, Int) = {
+
+    def lengthOfList[A](l:List[A]):Int = l match {
+      case Nil => 0
+      case h::tail => 1 + lengthOfList(tail)
+    }
+
+    val l = lengthOfList(list)
+    (list(l-1), lengthOfList(list))
+
+  }
+
+  def printTable(list: List[Long]): List[Long] = {
+
+   val index : List[Int] = (for(counter <- 1 to 10)
+     yield  counter).toList
+
+    index.flatMap(item => list.map(_*item))
+
+  }
+
+
+  def aggregateLists(list1: List[String], list2: List[Long]): List[List[(String, Long)]] = {
+
+    list1.zip(list2).map(item => List(item))
+
+  }
+
+  def getSumOfList(list: List[Long]): Long = list match{
+
+    case head :: tail => head + getSumOfList(tail)
+    case Nil => 0
+
+  }
+
+  def getMultiplicationOfList(list: List[Long]) : Long = list match{
+
+    case head :: tail => head * getSumOfList(tail)
+    case head :: Nil => 1
+    case Nil => 0
+
+  }
+
+  def quickSortList(list: List[Long]): List[Long] = list match {
+
+    case head :: tail => quickSortList(tail.filter(_< head)) ::: head :: quickSortList(tail.filter(_>=head))
+    case Nil => Nil
+
+  }
+
+  def mergeSortList(list: List[Long]):List[Long] = {
+
+    val mid = list.length/2
+    if (mid == 0){
+      list
+    }
+    else {
+      val (left, right) = list.splitAt(mid)
+      merge(mergeSortList(left), mergeSortList(right))
+    }
+  }
+
+  def merge(left: List[Long], right: List[Long]) : List[Long] =(left, right) match{
+
+    case (left,Nil) => left
+    case (Nil,right) => right
+    case (leftHead :: leftTail, righthead :: rightTail) => if(leftHead  < righthead){
+      leftHead :: merge(leftTail, right)
+    }
+    else {
+      righthead :: merge(left, rightTail)
+    }
+
+  }
 
 }
